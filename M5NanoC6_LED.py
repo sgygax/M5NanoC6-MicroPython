04acb155d4d2cf7a967f7798e3f7a1e9cdb05702
sgygax <stefan.gygax@gee.ch>
2025-07-14 20:30:14 +0200

    Update M5NanoC6_LED.py

--- M5NanoC6_LED.py
+++ M5NanoC6_LED.py
@@ -7,7 +7,7 @@ import machine
 
 #--------------------------------------------------
 # setup
-led_blue = machine.PWM(machine.Pin(7))
+led_blue = machine.Pin(7, machine.Pin.OUT)
 
 #--------------------------------------------------
 # loop
