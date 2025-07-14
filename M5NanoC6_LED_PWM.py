58152a54eba2bbb52b66cfa63933206b32756122
sgygax <stefan.gygax@gee.ch>
2025-07-14 20:15:48 +0200

    Add files via upload

---
+++ M5NanoC6_LED_PWM.py
@@ -0,0 +1,25 @@
+# The built-in blue LED and pulse-width modulation (PWM).
+# - https://docs.m5stack.com/en/core/M5NanoC6
+
+#--------------------------------------------------
+import time
+import machine
+
+#--------------------------------------------------
+# setup
+led_blue = machine.PWM(7)
+
+led_blue.duty(0)
+
+#--------------------------------------------------
+# loop
+while True:
+    # 0..100%
+    for tg in range(0, 1024, 1):
+        led_blue.duty(tg)
+        time.sleep(0.002)
+
+    # 100..0%
+    for tg in range(1023, -1, -1):
+        led_blue.duty(tg)
+        time.sleep(0.002)
